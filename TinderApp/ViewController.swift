5026076ee34f2a68d600cbd239d30dc37f152482
SergeyGladkiy <neofun10@mail.ru>
2020-02-10 13:43:48 +0300

    initial commit

---
+++ TinderApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderApp
+//
+//  Created by Serg on 06.02.2020.
+//  Copyright © 2020 Sergey Gladkiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
